4b08a0d56f3b2064bca9f33c49f13b8849c53540
hardik-4622 <1dinsanely@gmail.com>
2025-05-28 10:58:43 +0530

    Update pizza_sales_analysis_1.sql

--- pizza_sales_analysis_1.sql
+++ pizza_sales_analysis_1.sql
@@ -6,11 +6,27 @@ CREATE TABLE orders(
     order_time TIME NOT NULL
 );
 
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/orders.csv'
+INTO TABLE orders
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
 CREATE TABLE order_details(
 	order_detail_id INTEGER NOT NULL PRIMARY KEY,
     order_id INTEGER NOT NULL,
     pizza_id TEXT NOT NULL,
 	quantity INTEGER NOT NULL
 );
+
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/order_details.csv'
+INTO TABLE order_details
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
 
 SELECT * FROM pizza_types;
